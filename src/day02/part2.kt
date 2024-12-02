5f31c6f3c475c5d426043d389d0bafecf5ced49b
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-02 09:42:54 +0100

    fix with brute force

--- src/day02/part2.kt
+++ src/day02/part2.kt
@@ -3,7 +3,6 @@ package day02
 import puzzle
 
 fun main() {
-    safe("69 67 70 72 73 74 76")
     puzzle(4) { reports ->
         reports.count { report ->
             safe(report)
@@ -13,14 +12,17 @@ fun main() {
 
 private fun safe(report: String): Boolean {
     val levels = report.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
-    val safe = checkLevels(levels, true) || checkLevels(levels.drop(1), false)
+    val safe =
+        levels.filterIndexed { index, i ->
+            val list = levels.toMutableList()
+            list.removeAt(index)
+            checkLevels(list)
+        }.any()
     println("$report: $safe")
     return safe
 }
 
-private fun checkLevels(levels: List<Int>, tryAgain: Boolean): Boolean {
-    var backup: Int? = null
-    var backupUsed = !tryAgain
+private fun checkLevels(levels: List<Int>): Boolean {
     val range = if (levels[1] < levels[0]) {
         // decreasing order
         -3..-1
@@ -29,21 +31,7 @@ private fun checkLevels(levels: List<Int>, tryAgain: Boolean): Boolean {
         1..3
     }
 
-    fun check(list: List<Int>, index: Int): Boolean {
-        val f = backup ?: list[0]
-        backup = null
-        val safe = list[1] - f in range
-        return if (!safe && !backupUsed) {
-            backupUsed = true
-            backup = list[0]
-            true
-        } else {
-            safe
-        }
-    }
-
-    val safe = levels.windowed(2).mapIndexed { index, it ->
-        check(it, index)
+    return levels.windowed(2).mapIndexed { index, it ->
+        it[1] - it[0] in range
     }.all { it }
-    return safe
 }
