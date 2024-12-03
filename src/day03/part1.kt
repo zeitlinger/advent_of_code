b56b1dffc3baac63d39a12ec25749064eb00d348
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-03 06:57:37 +0100

    part1 done (realized I only checked the first line)

--- src/day03/part1.kt
+++ src/day03/part1.kt
@@ -3,7 +3,16 @@ package day03
 import puzzle
 
 fun main() {
-    puzzle(2) { lines ->
-        0
+    puzzle(161) { lines ->
+        val regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)".toRegex()
+        lines.map {
+            println(it)
+            regex.findAll(it).map {
+                val (a, b) = it.destructured
+                println(it.value)
+                println(it.range)
+                (a.toInt() * b.toInt()).also { println(it) }
+            }.sum()
+        }.sum()
     }
 }
