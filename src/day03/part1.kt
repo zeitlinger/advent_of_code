3262650a456c57f03bb487b2c851ae826f08d03b
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-03 06:26:27 +0100

    start day 3

---
+++ src/day03/part1.kt
@@ -0,0 +1,9 @@
+package day03
+
+import puzzle
+
+fun main() {
+    puzzle(2) { lines ->
+        0
+    }
+}
---
+++ src/day03/part2.kt
@@ -0,0 +1,10 @@
+package day02
+
+import puzzle
+
+fun main() {
+    puzzle(2) { lines ->
+        0
+    }
+}
+
