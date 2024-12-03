7401dee67d2cac451ddea259de84bffed24e3bf4
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-03 07:09:00 +0100

    part2 done just make sure that "don't" is not taken for "do"

--- src/day03/part2.kt
+++ src/day03/part2.kt
@@ -1,10 +1,45 @@
-package day02
+package day03
 
 import puzzle
 
 fun main() {
-    puzzle(2) { lines ->
-        0
+    puzzle(48) { lines ->
+        val regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)|do(n't)?\\(\\)".toRegex()
+        var enabled = true
+        lines.map {
+            println(it)
+            regex.findAll(it).map {
+                val s = it.groupValues[0]
+                println(s)
+                when {
+                    s.startsWith("mul") -> {
+                        val (a, b) = it.destructured
+                        (a.toInt() * b.toInt()).let {
+                            if (enabled) {
+                                println(it)
+                                it
+                            } else {
+                                0
+                            }
+                        }
+                    }
+
+                    s.startsWith("don't") -> {
+                        println("don't")
+                        enabled = false
+                        0
+                    }
+
+                    s.startsWith("do") -> {
+                        println("do")
+                        enabled = true
+                        0
+                    }
+
+                    else -> 0
+                }
+            }.sum()
+        }.sum()
     }
 }
 
