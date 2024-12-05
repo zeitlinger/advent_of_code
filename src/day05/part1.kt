8b66e3b4a7f21dd4c72760c816a30adfa99a9dd4
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-05 07:00:37 +0100

    part1 done

--- src/day05/part1.kt
+++ src/day05/part1.kt
@@ -1,10 +1,33 @@
 package day05
 
 import puzzle
-import kotlin.math.absoluteValue
 
 fun main() {
-    puzzle(11) {
-        0
+    puzzle(143) { lines ->
+        val rules = lines
+            .filter { "|" in it }
+            .map { it.split("|") }
+            .map { it[0].toInt() to it[1].toInt() }
+
+        fun isInRightOrder(updates: List<Int>): Boolean {
+            return rules.all {
+                val (a, b) = it
+                val aIndex = updates.indexOf(a)
+                val bIndex = updates.indexOf(b)
+
+                aIndex < 0 || bIndex < 0 || aIndex < bIndex
+            }
+        }
+
+        val updates = lines
+            .filter { "|" !in it }
+            .map { it.split(",").map { it.toInt() } }
+            .filter { isInRightOrder(it) }
+            .sumOf {
+                println("in order: $it")
+                it[it.size / 2]
+            }
+
+        updates
     }
 }
