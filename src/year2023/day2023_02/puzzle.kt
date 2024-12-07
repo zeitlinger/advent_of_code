4fb818ce5eae420822da9b06c42b37925fbd39b6
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-07 15:24:53 +0100

    part1 done

--- src/year2023/day2023_02/puzzle.kt
+++ src/year2023/day2023_02/puzzle.kt
@@ -3,7 +3,24 @@ package year2023.day2023_02
 import puzzle
 
 fun main() {
-    puzzle(281) { lines ->
-        0
+    puzzle(8) { lines ->
+        // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        lines.sumOf { line ->
+            val (a, b) = line.split(":")
+            val game = a.split(" ").let { it[1].toInt() }
+            val sets = b.split(";")
+            val possible = sets.all {
+                it.split(",").all {
+                    val (count, color) = it.trim().split(" ")
+                    when (color) {
+                        "blue" -> count.toInt() <= 14
+                        "red" -> count.toInt() <= 12
+                        "green" -> count.toInt() <= 13
+                        else -> false
+                    }
+                }
+            }
+            if (possible) game else 0
+        }
     }
 }
