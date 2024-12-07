dbf69c98468cc7c9a8e74421839c0892c028d1b4
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-07 13:13:20 +0100

    part2 done

--- src/year2023/day2023_01/puzzle.kt
+++ src/year2023/day2023_01/puzzle.kt
@@ -3,9 +3,33 @@ package year2023.day2023_01
 import puzzle
 
 fun main() {
-    puzzle(142) { lines ->
-         lines.sumOf {
-             (it.first { it.isDigit() }.toString() + it.last { it.isDigit() }).toInt()
+    puzzle(281) { lines ->
+         lines.sumOf { line ->
+             val digits = line.indices.mapNotNull { start ->
+                 digit(line.substring(start))
+             }
+             digits.first() * 10 + digits.last()
          }
+    }
+}
+
+enum class Digit {
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine
+}
+
+private fun digit(s: String): Int? = when {
+    s.first().isDigit() -> s.first().digitToInt()
+    else -> {
+        Digit.entries.firstOrNull {
+            s.startsWith(it.toString())
+        }?.let { it.ordinal + 1 }
     }
 }
