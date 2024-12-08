a07b49d9440435d6ff83b4f7430ac7738aeac5c3
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-08 14:41:31 +0100

    cont

--- src/year2023/day2023_05/puzzle.kt
+++ src/year2023/day2023_05/puzzle.kt
@@ -7,6 +7,6 @@ data class Mapping(val SrcFrom: Int, val dstFrom: Int, val length: Int)
 fun main() {
     puzzle(35) { lines ->
         val seeds = lines[0].substringAfter(":").trim().split(" ").map { it.toInt() }
-        0
+        1
     }
 }
