1c8fb00b5d95c434cb55194ffa02e4fc246fb544
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-08 10:12:17 +0100

    part1 done

--- src/year2023/day2023_04/puzzle.kt
+++ src/year2023/day2023_04/puzzle.kt
@@ -1,9 +1,16 @@
 package year2023.day2023_04
 
 import puzzle
+import kotlin.math.pow
 
 fun main() {
-    puzzle(-1) { lines ->
-        0
+    puzzle(13) { lines ->
+        lines.sumOf { game ->
+            val (winning, have) = game.split(":")[1].trim().split("|")
+            val winningNumbers = winning.trim().split(" ").filter { it.isNotBlank() }.map { it.trim().toInt() }.toSet()
+            val haveNumbers = have.trim().split(" ").filter { it.isNotBlank() }.map { it.trim().toInt() }
+            val score = haveNumbers.count { it in winningNumbers }
+            if (score == 0) 0 else 2.0.pow(score).toInt() / 2
+        }
     }
 }
