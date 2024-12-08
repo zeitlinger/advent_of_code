a19bd5a9be1f2daf3f57444cb7c8b6efc852be04
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-08 08:21:37 +0100

    part2 done

--- src/year2024/day2024_08/puzzle.kt
+++ src/year2024/day2024_08/puzzle.kt
@@ -7,7 +7,7 @@ data class Point(val x: Int, val y: Int)
 data class Antenna(val loc: Point, val frequency: Char)
 
 fun main() {
-    puzzle(14) { lines ->
+    puzzle(34) { lines ->
         val maxX = lines.maxOf { it.length }
         val maxY = lines.size
         val uniqueLocations = mutableSetOf<Point>()
@@ -24,14 +24,22 @@ fun main() {
                         .forEach { other ->
                             val dx = antenna.loc.x - other.loc.x
                             val dy = antenna.loc.y - other.loc.y
-                            val targetX = other.loc.x + 2 * dx
-                            val targetY = other.loc.y + 2 * dy
-                            if (targetX in 0 until maxX && targetY in 0 until maxY) {
+                            var targetX = other.loc.x + dx
+                            var targetY = other.loc.y + dy
+                            while (targetX in 0 until maxX && targetY in 0 until maxY) {
                                 uniqueLocations.add(Point(targetX, targetY))
+                                targetX += dx
+                                targetY += dy
                             }
                         }
                 }
             }
+        val net = lines.mapIndexed { y, line ->
+            line.mapIndexed { x, c ->
+                if (Point(x, y) in uniqueLocations) '#' else c
+            }.joinToString("")
+        }.joinToString("\n")
+        println(net)
         uniqueLocations.size
     }
 }
