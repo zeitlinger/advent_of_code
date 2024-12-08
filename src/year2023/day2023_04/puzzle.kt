33083256ad84684f38f9ed69857519712debb2d4
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-08 14:05:38 +0100

    previous solution was right - no idea why it was rejected

--- src/year2023/day2023_04/puzzle.kt
+++ src/year2023/day2023_04/puzzle.kt
@@ -2,45 +2,22 @@ package year2023.day2023_04
 
 import puzzle
 
-data class Copy(val cardNum: Int, val score: Int, var processed: Boolean = false)
-
 fun main() {
     puzzle(30) { lines ->
-        val copies = MutableList<MutableList<Copy>>(lines.size) { mutableListOf() }
-
-        fun addCopies(
-            cardNum: Int,
-            score: Int
-        ) {
-               for (i in lines.indices) {
-                   if (i > cardNum && i <= score + cardNum) {
-                       copies[i].add(Copy(cardNum, score))
-                   }
-               }
-           }
-
-        fun processFirstUnprocessed(copies: List<MutableList<Copy>>): Boolean {
-            val firstUnprocessed = copies.indexOfFirst { it.any { !it.processed } }
-            if (firstUnprocessed == -1) {
-                return false
-            }
-            val copy = copies[firstUnprocessed].first { !it.processed }
-            copy.processed = true
-            addCopies(copy.cardNum, copy.score)
-            return true
-        }
+        val copyCount = MutableList(lines.size) { 1 }
 
         lines.forEachIndexed { cardNum, game ->
             val (winning, have) = game.split(":")[1].trim().split("|")
             val winningNumbers = winning.trim().split(" ").filter { it.isNotBlank() }.map { it.trim().toInt() }.toSet()
             val haveNumbers = have.trim().split(" ").filter { it.isNotBlank() }.map { it.trim().toInt() }
             val score = haveNumbers.count { it in winningNumbers }
-            addCopies(cardNum, score)
+            val copies = copyCount[cardNum]
+            for (i in lines.indices) {
+                if (i > cardNum && i <= score + cardNum) {
+                    copyCount[i] += copies
+                }
+            }
         }
-
-        while (processFirstUnprocessed(copies)) {
-            // process all
-        }
-        copies.sumOf { it.count() + 1 }
+        copyCount.sum()
     }
 }
