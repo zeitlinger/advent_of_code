a5eb2dcb073327207b1fc389b938f6e41fb56840
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-09 06:57:50 +0100

    start

--- src/year2023/day2023_05/puzzle.kt
+++ src/year2023/day2023_05/puzzle.kt
@@ -2,16 +2,29 @@ package year2023.day2023_05
 
 import puzzle
 
-data class Mapping(val srcFrom: Long, val dstFrom: Long, val length: Long)
+data class Mapping(val srcFrom: Long, val dstFrom: Long, val length: Long) {
+//    fun intersection(range: LongRange) {
+//        val srcRange = srcFrom until srcFrom + length
+//        val dstRange = dstFrom until dstFrom + length
+//        if (srcRange.intersect(range) != ) {
+//            throw IllegalArgumentException("intersection $srcRange $dstRange $range")
+//        }
+//    }
+}
 
 data class Rule(val src: String, val dst: String, val mappings: MutableList<Mapping>)
 
-data class Product(val type: String, val ids: List<Long>)
+data class Product(val type: String, val ids: List<LongRange>)
 
 fun main() {
-    puzzle(35) { lines ->
+    puzzle(46) { lines ->
         val headerRegex = "(.+)-to-(.+) map:".toRegex()
         val seeds = lines[0].substringAfter(":").trim().split(" ").map { it.toLong() }
+            .chunked(2)
+            .map {
+                val from = it[0]
+                from until from + it[1]
+            }
         val rules = mutableListOf<Rule>()
         lines.drop(1).forEach { line ->
             val header = headerRegex.matchEntire(line)
@@ -27,15 +40,23 @@ fun main() {
 
         fun translate(input: Product): Product {
             val rule = ruleMap[input.type] ?: return input
-            val newIds = input.ids.map { srcId ->
-                val newId = rule.mappings.firstNotNullOfOrNull { mapping ->
-                    if (srcId in mapping.srcFrom until mapping.srcFrom + mapping.length) {
-                        mapping.dstFrom + srcId - mapping.srcFrom
-                    } else {
-                        null
-                    }
+            val newIds = input.ids.map { srcRange ->
+                val newRange = srcRange.toMutableList()
+                rule.mappings.forEach { mapping ->
+
                 }
-                newId ?: srcId
+//                srcId.contains()
+//                val newId = rule.mappings.firstNotNullOfOrNull { mapping ->
+//
+//
+//                    if (srcId in mapping.srcFrom until mapping.srcFrom + mapping.length) {
+//                        mapping.dstFrom + srcId - mapping.srcFrom
+//                    } else {
+//                        null
+//                    }
+//                }
+//                newId ?: srcId
+                srcRange
             }
             println("translate $input to $newIds")
             return translate(Product(rule.dst, newIds))
@@ -45,6 +66,7 @@ fun main() {
         if (location.type != "location") {
             throw IllegalArgumentException("not a location: $location")
         }
-        location.ids.min()!!
+//        location.ids.min()!!
+        0
     }
 }
---
+++ src/year2024/day2024_100_template/puzzle.kt
@@ -0,0 +1,9 @@
+package year2024.day2024_100_template
+
+import puzzle
+
+fun main() {
+    puzzle(-1) { lines ->
+        0
+    }
+}
