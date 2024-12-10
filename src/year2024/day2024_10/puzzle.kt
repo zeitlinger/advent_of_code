74384eb9902c4524d693a09b21df48757559da97
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-10 07:58:11 +0100

    cont

--- src/year2024/day2024_10/puzzle.kt
+++ src/year2024/day2024_10/puzzle.kt
@@ -2,8 +2,21 @@ package year2024.day2024_10
 
 import puzzle
 
+data class Point(val x: Int, val y: Int)
+
+enum class Direction(val dx: Int, val dy: Int) {
+    UP(0, -1),
+    DOWN(0, 1),
+    LEFT(-1, 0),
+    RIGHT(1, 0)
+}
+
 fun main() {
-    puzzle(-1) { lines ->
+    puzzle(36) { lines ->
+        fun score(point: Point): Int {
+            0
+        }
+
         0
     }
 }
