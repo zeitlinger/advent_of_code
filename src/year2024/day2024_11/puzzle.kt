7f8074efa6da67ff1096d5b0b2928fa195a3d54d
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-11 06:53:53 +0100

    part1 done

--- src/year2024/day2024_11/puzzle.kt
+++ src/year2024/day2024_11/puzzle.kt
@@ -3,7 +3,26 @@ package year2024.day2024_11
 import puzzle
 
 fun main() {
-    puzzle(-1) { lines ->
-        0
+    puzzle(55312) { lines ->
+        var stones = lines[0].split(" ").map { it.toLong() }.toMutableList()
+        for (blink in 1..25) {
+            val newStones = mutableListOf<Long>()
+            for (value in stones) {
+                val s = value.toString()
+                when {
+                    value == 0L -> newStones.add(1)
+                    s.length.mod(2) == 0 -> {
+                        newStones.add(s.substring(0, s.length / 2).toLong())
+                        newStones.add(s.substring(s.length / 2).toLong())
+                    }
+                    else -> {
+                        newStones.add(value * 2024)
+                    }
+                }
+            }
+            stones = newStones
+//            println(stones)
+        }
+        stones.size
     }
 }
