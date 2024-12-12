b2fb55770f6fa7e8aa6e8ffbb263726e8bb72bef
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-12 07:05:28 +0100

    part1 done

--- src/year2024/day2024_12/puzzle.kt
+++ src/year2024/day2024_12/puzzle.kt
@@ -15,34 +15,41 @@ enum class Direction(val dx: Int, val dy: Int) {
 
 fun main() {
     puzzle(1930) { lines ->
-        val maxX = lines.maxOf { it.length }
+        val input = lines.map { it.toCharArray().toMutableList() }
+        val maxX = input.maxOf { it.size }
         var regions = 0
-        val regionMap: MutableList<MutableList<Region?>> = MutableList(lines.size) { MutableList(maxX) { null } }
+        val regionMap: MutableList<MutableList<Region?>> = MutableList(input.size) { MutableList(maxX) { null } }
 
-        fun addRegion(letter: Char, x: Int, y: Int) {
-            val old = Direction.entries.firstOrNull {
+        fun addRegion(x: Int, y: Int, region: Region) {
+            region.gardens += Point(x, y)
+            regionMap[y][x] = region
+            input[y][x] = '.'
+
+            Direction.entries.forEach {
                 val nx = x + it.dx
                 val ny = y + it.dy
-                regionMap.getOrNull(ny)?.getOrNull(nx)?.letter == letter
-            }?.let { regionMap[y + it.dy][x + it.dx] }
-
-            if (old != null) {
-                regionMap[y][x] = old
-                old.gardens += Point(x, y)
-            } else {
-                regionMap[y][x] = Region(regions++, letter, mutableListOf(Point(x, y)))
+                if (input.getOrNull(ny)?.getOrNull(nx) == region.letter) {
+                    addRegion(nx, ny, region)
+                }
             }
         }
 
         fun price(region: Region): Long {
-            println(region)
-            return 0
+            val fenceLen = region.gardens.sumOf { point ->
+                Direction.entries.count { dir ->
+                    val nx = point.x + dir.dx
+                    val ny = point.y + dir.dy
+                    regionMap.getOrNull(ny)?.getOrNull(nx) != region
+                }
+            }
+            return fenceLen.toLong() * region.gardens.size
         }
 
-        lines.forEachIndexed { y, line ->
-            line.forEachIndexed { x, letter ->
+        input.forEachIndexed { y, line ->
+            line.forEachIndexed { x, _ ->
+                val letter = input[y][x]
                 if (letter != '.') {
-                    addRegion(letter, x, y)
+                    addRegion(x, y, Region(regions++, letter, mutableListOf()))
                 }
             }
         }
