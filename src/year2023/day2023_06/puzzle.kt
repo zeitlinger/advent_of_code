f7bc1f45a11eb96991e489c648069ebc031f61f1
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-12 17:45:42 +0100

    part 2 done

--- src/year2023/day2023_06/puzzle.kt
+++ src/year2023/day2023_06/puzzle.kt
@@ -2,7 +2,7 @@ package year2023.day2023_06
 
 import puzzle
 
-data class RecordRace(val time: Int, val distance: Int)
+data class RecordRace(val time: Long, val distance: Long)
 
 fun bisectSmallest(range: LongRange, predicate: (Long) -> Boolean): Long {
     var left = range.first
@@ -32,29 +32,27 @@ fun bisectLargest(range: LongRange, predicate: (Long) -> Boolean): Long {
     return left
 }
 
-fun waysToBeat(recordRace: RecordRace): Int {
+fun waysToBeat(recordRace: RecordRace): Long {
     val predicate: (Long) -> Boolean = { chargeTime ->
        chargeTime * (recordRace.time - chargeTime) > recordRace.distance
     }
-    val range = 1L until recordRace.time.toLong()
+    val range = 1L until recordRace.time
     val smallest = bisectSmallest(range, predicate)
     val largest = bisectLargest(range, predicate)
-    return (largest - smallest + 1).toInt()
+    return (largest - smallest + 1)
 }
 
 fun main() {
-    puzzle(288) { lines ->
-        val times = lines[0]
+    puzzle(71503) { lines ->
+        val time = lines[0]
             .substringAfter(":")
-            .split(" ")
-            .filter { it.isNotBlank() }
-            .map { it.toInt() }
-        val distances = lines[1]
+            .replace(" ", "")
+            .toLong()
+        val distance = lines[1]
             .substringAfter(":")
-            .split(" ")
-            .filter { it.isNotBlank() }
-            .map { it.toInt() }
-        val records: List<RecordRace> = times.zip(distances).map { RecordRace(it.first, it.second) }
+            .replace(" ", "")
+            .toLong()
+        val records: List<RecordRace> = listOf(RecordRace(time, distance))
 
         records.map { waysToBeat(it) }.reduce { acc, i -> acc * i }
     }
