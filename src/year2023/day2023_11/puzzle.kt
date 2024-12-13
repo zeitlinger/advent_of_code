d3a5871285a149be4c390f901da057504beb62e4
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-13 18:25:15 +0100

    part 2 done

--- src/year2023/day2023_11/puzzle.kt
+++ src/year2023/day2023_11/puzzle.kt
@@ -2,16 +2,14 @@ package year2023.day2023_11
 
 import puzzle
 import kotlin.math.absoluteValue
-import kotlin.math.roundToInt
-import kotlin.math.sqrt
 
-data class Point(val x: Int, val y: Int) {}
+data class Point(var x: Long, var y: Long) {}
 
 data class Galaxy(val id: Int, val location: Point)
 
 fun main() {
-    puzzle(374) { lines ->
-        val galaxyBeforeExpansion = lines.map { it.toMutableList() }.toMutableList()
+    puzzle(null) { lines ->
+        val galaxyBeforeExpansion = lines.map { it.toList() }
         val galaxy = expandGalaxy(galaxyBeforeExpansion)
         // permutations of pairs
         val pairs = galaxy.map { g1 -> galaxy.map { g2 -> g1 to g2 } }
@@ -21,40 +19,58 @@ fun main() {
         val distances = pairs.map { (g1, g2) ->
             val dx = g1.location.x - g2.location.x
             val dy = g1.location.y - g2.location.y
-//            val d = sqrt((dx * dx + dy * dy).toDouble()).roundToInt()
             val d = dx.absoluteValue + dy.absoluteValue
-            println("Distance between ${g1.id} and ${g2.id} is $d")
+//            println("Distance between ${g1.id} and ${g2.id} is $d")
             d
         }.sum()
         distances
     }
 }
 
-fun expandGalaxy(galaxy: MutableList<MutableList<Char>>): List<Galaxy> {
-    galaxy.indices.reversed().forEach { y ->
-        if (!galaxy[y].contains('#')) {
-            galaxy.add(y, galaxy[y].toMutableList())
-        }
-    }
-    galaxy[0].indices.reversed().forEach { x ->
-        if (galaxy.all { it[x] == '.' }) {
-            galaxy.forEachIndexed { y, it ->
-                it.add(x, '.')
-            }
-        }
-    }
-
-    println(galaxy.map { it.joinToString("") }.joinToString("\n"))
+fun expandGalaxy(galaxy: List<List<Char>>): List<Galaxy> {
+//    println(galaxy.map { it.joinToString("") }.joinToString("\n"))
 
     var id = 0
-    return galaxy.mapIndexedNotNull { y, row ->
+    val galaxies = galaxy.mapIndexed { y, row ->
         row.mapIndexedNotNull { x, c ->
             if (c == '#') {
                 id++
-                Galaxy(id, Point(x, y))
+                Galaxy(id, Point(x.toLong(), y.toLong()))
             } else {
                 null
             }
         }
     }.flatten()
+
+    val add = 1_000_000 - 1
+
+    galaxy.indices.reversed().forEach { y ->
+        if (galaxies.all { it.location.y != y.toLong() }) {
+            galaxies.filter { it.location.y > y }.forEach { it.location.y += add }
+        }
+    }
+    galaxy[0].indices.reversed().forEach { x ->
+        if (galaxies.all { it.location.x != x.toLong() }) {
+            galaxies.filter { it.location.x > x }.forEach { it.location.x += add }
+        }
+    }
+
+//    print(galaxies)
+
+    return galaxies
+}
+
+private fun print(galaxies: List<Galaxy>) {
+    for (y in 0..galaxies.maxOf { it.location.y }) {
+        val list = galaxies.filter { it.location.y == y }
+        if (list.isEmpty()) {
+            println()
+            continue
+        }
+        for (x in 0..list.maxOf { it.location.x }) {
+            val g = galaxies.find { it.location.x == x && it.location.y == y }
+            print(g?.id ?: '.')
+        }
+        println()
+    }
 }
