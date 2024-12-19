35a631a22fa3f69d421e4df6e5a9e61b7c46ce97
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-19 21:57:40 +0100

    part 2 done

--- src/year2023/day2023_15/puzzle.kt
+++ src/year2023/day2023_15/puzzle.kt
@@ -2,11 +2,41 @@ package year2023.day2023_15
 
 import puzzle
 
-fun main() {
-    puzzle(1320) { lines ->
-        lines[0].split(",").sumOf {
-            hash(it)
+data class Lens(val label: String, val focalLength: Int)
+
+data class HashMap(val boxes: MutableList<MutableList<Lens>> = MutableList(256) { mutableListOf() }) {
+    fun addLens(lens: Lens) {
+        val box = boxes[hash(lens.label)]
+        val slot = box.indexOfFirst { it.label == lens.label }
+        if (slot != -1) {
+            box[slot] = lens
+            return
         }
+        box.add(lens)
+    }
+
+    fun removeLens(label: String) {
+        boxes[hash(label)].removeAll { it.label == label }
+    }
+}
+
+fun main() {
+    puzzle(145) { lines ->
+        val hashMap = HashMap()
+        lines[0].split(",").forEach { command ->
+            if (command.endsWith("-")) {
+                val label = command.dropLast(1)
+                hashMap.removeLens(label)
+            } else {
+                val s = command.split("=")
+                val label = s[0]
+                val focalLength = s[1].toInt()
+                hashMap.addLens(Lens(label, focalLength))
+            }
+        }
+        hashMap.boxes.mapIndexed { box, list ->
+            list.mapIndexed { slot, lens -> lens.focalLength * (box + 1) * (slot + 1) }
+        }.flatten().sum()
     }
 }
 
