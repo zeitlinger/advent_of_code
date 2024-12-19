fa51e0ec038f5d1808f356b1a228a75bc458cebf
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-19 21:10:05 +0100

    part 2 done

--- src/year2023/day2023_14/puzzle.kt
+++ src/year2023/day2023_14/puzzle.kt
@@ -1,6 +1,6 @@
 package year2023.day2023_14
 
-import Point
+import Direction
 import puzzle
 
 enum class Content(val symbol: Char) {
@@ -9,58 +9,90 @@ enum class Content(val symbol: Char) {
     ROUND('O'),
 }
 
-data class Tile(val content: Content, val point: Point)
+data class Tile(val content: Content)
+
+data class Field(val tiles: MutableList<MutableList<Tile>>) {
+    fun copy(): Field {
+        return Field(tiles.map { it.toMutableList() }.toMutableList())
+    }
+}
 
 fun main() {
-    puzzle(136) { lines ->
-        val field = lines.mapIndexed { y, row ->
-            row.mapIndexedNotNull { x, c ->
-                Tile(Content.entries.first { it.symbol == c }, Point(x, y))
+    val directions = listOf(Direction.UP, Direction.LEFT, Direction.DOWN, Direction.RIGHT)
+    puzzle(64) { lines ->
+        val field = Field(lines.map { row ->
+            row.map { c ->
+                Tile(Content.entries.first { it.symbol == c })
             }.toMutableList()
-        }.toMutableList()
+        }.toMutableList())
         printField(field)
-        tiltUp(field)
-        printField(field)
+        var cycle = 0
+        val history = mutableListOf<Field>()
+        while (true) {
+            val needed = 1000000000
+            if (cycle == needed) {
+                break
+            }
+            if (history.contains(field)) {
+                val index = history.indexOf(field)
+                val cycleLength = cycle - index
+                val remaining = needed - cycle
+                val offset = remaining % cycleLength
+                return@puzzle totalLoad(history[index + offset])
+            }
+
+            history.add(field.copy())
+            directions.forEach { direction ->
+                tiltAll(field, direction)
+                println("Moved $direction")
+                printField(field)
+            }
+            cycle++
+        }
         totalLoad(field)
     }
 }
 
-fun printField(field: MutableList<MutableList<Tile>>) {
-    field.forEach { row ->
+fun printField(field: Field) {
+    field.tiles.forEach { row ->
         println(row.joinToString("") { it.content.symbol.toString() })
     }
     println()
 }
 
-fun tiltUp(field: MutableList<MutableList<Tile>>) {
-  field.forEachIndexed { y, row ->
-    row.forEachIndexed { x, tile ->
-      if (tile.content == Content.ROUND) {
-          tiltRound(field, y, x, tile)
-      }
-    }
-  }
-}
-
-private fun tiltRound(
-    field: MutableList<MutableList<Tile>>,
-    y: Int,
-    x: Int,
-    tile: Tile
-) {
-    val lastRow = field.getOrNull(y - 1)
-    if (lastRow != null) {
-        val nextTile = lastRow.getOrNull(x)
-        if (nextTile != null && nextTile.content == Content.EMPTY) {
-            lastRow[x] = Tile(Content.ROUND, nextTile.point)
-            field[y][x] = Tile(Content.EMPTY, tile.point)
-            tiltRound(field, y - 1, x, nextTile)
+fun tiltAll(field: Field, direction: Direction) {
+    val tiles = if (direction == Direction.DOWN) field.tiles.indices.reversed() else field.tiles.indices
+    tiles.forEach { y ->
+        val row = field.tiles[y]
+        val list = if (direction == Direction.RIGHT) row.indices.reversed() else row.indices
+        list.forEach { x ->
+            val tile = field.tiles[y][x]
+            if (tile.content == Content.ROUND) {
+                tiltRound(field, y, x, direction)
+            }
         }
     }
 }
 
-fun totalLoad(field: MutableList<MutableList<Tile>>): Int {
-    return field.mapIndexed { index, tiles ->
-        tiles.count { it.content == Content.ROUND } * (field.size - index)
+private fun tiltRound(
+    field: Field,
+    y: Int,
+    x: Int,
+    direction: Direction,
+) {
+    val lastRow = field.tiles.getOrNull(y + direction.y)
+    if (lastRow != null) {
+        val nextTile = lastRow.getOrNull(x + direction.x)
+        if (nextTile != null && nextTile.content == Content.EMPTY) {
+            lastRow[x + direction.x] = Tile(Content.ROUND)
+            field.tiles[y][x] = Tile(Content.EMPTY)
+            tiltRound(field, y + direction.y, x + direction.x, direction)
+        }
+    }
+}
+
+fun totalLoad(field: Field): Int {
+    return field.tiles.mapIndexed { index, tiles ->
+        tiles.count { it.content == Content.ROUND } * (field.tiles.size - index)
     }.sum()
 }
