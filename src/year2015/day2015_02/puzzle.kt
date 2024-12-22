ca2734d37026a58bb394a0e5a32b7be3b95522c4
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-22 17:06:22 +0100

    part 2 done

--- src/year2015/day2015_02/puzzle.kt
+++ src/year2015/day2015_02/puzzle.kt
@@ -3,13 +3,14 @@ package year2015.day2015_02
 import puzzle
 
 fun main() {
-//    A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
-    puzzle(58) { lines ->
+//    A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+    puzzle(34) { lines ->
         lines.sumOf {
-            val (l, w, h) = it.split("x").map { it.toInt() }
-            val sides = listOf(l * w, w * h, h * l)
-            val slack = sides.minOrNull()!!
-            2 * sides.sum() + slack
+            val dimensions = it.split("x").map { it.toInt() }
+            val (l, w, h) = dimensions
+            val wrap = 2 * dimensions.sorted().take(2).sum()
+            val bow = l * w * h
+            wrap + bow
         }
     }
 }
