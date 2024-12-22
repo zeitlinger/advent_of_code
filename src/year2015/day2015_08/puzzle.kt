8e83d6d86b6185a14a63ee6bc09d3dd8391adec8
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-22 22:17:03 +0100

    part 2 done

--- src/year2015/day2015_08/puzzle.kt
+++ src/year2015/day2015_08/puzzle.kt
@@ -3,28 +3,15 @@ package year2015.day2015_08
 import puzzle
 
 fun main() {
-    puzzle(12) { lines ->
-        val stringSize = lines.sumOf { it.length }
+    puzzle(19) { lines ->
         val memorySize = lines.sumOf {
-            memorySize(it)
+            it.length
         }
+        val stringSize = lines.sumOf { encode(it).length }
         stringSize - memorySize
     }
 }
 
-val regex = Regex("""(\\x[0-9a-f]{2}|\\"|\\\\)""")
-
-private fun memorySize(s: String): Int = when {
-    s.startsWith("\"") -> memorySize(s.substring(1, s.length - 1))
-    else -> {
-        var result = s.length
-        regex.findAll(s).forEach {
-            result -= when  {
-                it.value.startsWith("\\x") -> 3
-                else -> 1
-            }
-        }
-        result
-    }
-
+fun encode(s: String): String {
+    return "\"${s.replace(Regex("""(\\|")""")) { "\\${it.value}" }}\""
 }
