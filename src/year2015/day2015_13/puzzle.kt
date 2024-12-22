ac56e984dbcf28d41e29c7b6b514c43eca32b680
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-22 23:21:02 +0100

    part 2 done

--- src/year2015/day2015_13/puzzle.kt
+++ src/year2015/day2015_13/puzzle.kt
@@ -9,13 +9,21 @@ val regex = Regex("(.+) would (gain|lose) (\\d+) happiness units by sitting next
 data class Constraint(val a: String, val b: String, val mood: Int)
 
 fun main() {
-    puzzle(330) { lines ->
+    puzzle(null) { lines ->
         val constraints = lines.map {
             regex.find(it)!!.destructured.let { (a, sign, n, b) ->
                 Constraint(a, b, if (sign == "gain") n.toInt() else -n.toInt())
             }
+        }.toMutableList()
+
+        val others = constraints.flatMap { listOf(it.a, it.b) }.toSet()
+        others.forEach { person ->
+            constraints.add(Constraint("Me", person, 0))
+            constraints.add(Constraint(person, "Me", 0))
         }
+
         val people = constraints.flatMap { listOf(it.a, it.b) }.toSet()
+
         people.permutations().maxOf { seating ->
             seating.windowed(2).sumOf { (a, b) ->
                 constraints.filter { it.a == a && it.b == b || it.a == b && it.b == a }.sumOf { it.mood }
