a3b2f29393aa65acf863d6a8e2d6896bc99025da
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-22 17:41:27 +0100

    part 2 done

--- src/year2015/day2015_06/puzzle.kt
+++ src/year2015/day2015_06/puzzle.kt
@@ -6,35 +6,39 @@ val regex = Regex("(turn on|turn off|toggle) (\\d+),(\\d+) through (\\d+),(\\d+)
 
 fun main() {
     puzzle(null) { lines ->
-        val lights = Array(1000) { BooleanArray(1000) }
+        val lights = Array(1000) { Array(1000) { 0 } }
         lines.forEach { instruction ->
             println(instruction)
             val match = regex.find(instruction) ?: throw IllegalArgumentException("Invalid instruction: $instruction")
             val (action, x1, y1, x2, y2) = match.destructured
-            when(action) {
+            when (action) {
                 "turn on" -> {
                     for (x in x1.toInt()..x2.toInt()) {
                         for (y in y1.toInt()..y2.toInt()) {
-                            lights[x][y] = true
+                            lights[x][y] += 1
                         }
                     }
                 }
+
                 "turn off" -> {
                     for (x in x1.toInt()..x2.toInt()) {
                         for (y in y1.toInt()..y2.toInt()) {
-                            lights[x][y] = false
+                            if (lights[x][y] > 0) {
+                                lights[x][y] -= 1
+                            }
                         }
                     }
                 }
+
                 "toggle" -> {
                     for (x in x1.toInt()..x2.toInt()) {
                         for (y in y1.toInt()..y2.toInt()) {
-                            lights[x][y] = !lights[x][y]
+                            lights[x][y] += 2
                         }
                     }
                 }
             }
         }
-        lights.sumOf { it.count { it } }
+        lights.sumOf { it.sumOf { it } }
     }
 }
