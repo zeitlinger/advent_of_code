14f5faf4025b07cb71ae7f8d09d40d87bfb10b49
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-24 10:34:04 +0100

    wip

--- src/year2024/day2024_24/puzzle.kt
+++ src/year2024/day2024_24/puzzle.kt
@@ -1,6 +1,6 @@
 package year2024.day2024_24
 
-import puzzle
+import PuzzleRun
 import stringPuzzle
 
 //tnw OR pbm -> gnj
@@ -11,41 +11,65 @@ val valueRegex = Regex("""(\w+): (\d+)""")
 
 fun main() {
     stringPuzzle(null) { input ->
-        val wires = input.lines.map { instruction ->
-            val literal = valueRegex.matchEntire(instruction)
-            val op = opRegex.matchEntire(instruction)
-            when {
-                literal != null -> {
-                    val (name, value) = literal.destructured
-                    Wire(
-                        name,
-                        instruction,
-                        value.toLong(),
-                        emptyList()
-                    ) { throw IllegalArgumentException("Should not be called") }
-                }
+        val wires = readInput(input)
+//        printTree(wires)
+        fixWires(wires)
+//        simulateWires(wires)
+//        wireOutput(wires)
+        "todo"
+    }
+}
 
-                op != null -> {
-                    val (x, op, y, name) = op.destructured
-                    Wire(name, instruction, null, listOf(x, y)) { vals ->
-                        when (op) {
-                            "AND" -> vals[0] and vals[1]
-                            "OR" -> vals[0] or vals[1]
-                            "XOR" -> vals[0] xor vals[1]
-                            else -> throw IllegalArgumentException("Unknown operation $op")
-                        }
-                    }
-                }
+private fun readInput(input: PuzzleRun) = input.lines.map { instruction ->
+    val literal = valueRegex.matchEntire(instruction)
+    val operation = opRegex.matchEntire(instruction)
+    when {
+        literal != null -> {
+            val (name, value) = literal.destructured
+            Wire(
+                name,
+                instruction,
+                "=",
+                value.toLong(),
+                emptyList()
+            ) { throw IllegalArgumentException("Should not be called") }
+        }
 
-                else -> {
-                    val (name, x) = instruction.split(" -> ")
-                    Wire(name, instruction, null, listOf(x)) { it[0] }
+        operation != null -> {
+            val (x, op, y, name) = operation.destructured
+            Wire(name, instruction, op, null, listOf(x, y)) { vals ->
+                when (op) {
+                    "AND" -> vals[0] and vals[1]
+                    "OR" -> vals[0] or vals[1]
+                    "XOR" -> vals[0] xor vals[1]
+                    else -> throw IllegalArgumentException("Unknown operation $op")
                 }
             }
         }
-        printTree(wires)
-        simulateWires(wires)
-        wireOutput(wires)
+
+        else -> throw IllegalArgumentException("Unknown instruction $instruction")
+    }
+}
+
+fun fixWires(wires: List<Wire>) {
+    val wireMap = wires.associateBy { it.name }
+    traceWire(0, wireMap)
+}
+
+fun traceWire(bit: Int, wireMap: Map<String, Wire>) {
+    println("trace bit: $bit")
+    if (bit > 0) {
+        // todo find carry-in
+    }
+    val num = bit.toString().padStart(2, '0')
+    if (bit < 45) {
+        // todo special case
+        val x = wireMap["x$num"]!!
+        val y = wireMap["y$num"]!!
+        val inOxr = wireMap.values.find { it.op == "XOR" && it.needs.contains(x.name) && it.needs.contains(y.name) }!!
+    }
+    if (bit < 45) {
+        traceWire(bit + 1, wireMap)
     }
 }
 
@@ -72,10 +96,10 @@ fun wireOutput(wires: List<Wire>): Long {
         .sum()
 }
 
-
 data class Wire(
     val name: String,
     val instruction: String,
+    val op: String,
     var value: Long?,
     val needs: List<String>,
     val operation: (List<Long>) -> Long
@@ -90,57 +114,6 @@ fun simulateWires(wireList: List<Wire>) {
 
     val wires =
         wireList.associateBy { it.name }
-
-//        val wires =
-//            lines.map { instruction ->
-//                val (command, wire) = instruction.split(" -> ")
-//                when {
-//                    command.toIntOrNull() != null ->
-//                        Wire(
-//                            wire,
-//                            instruction,
-//                            command.toInt(),
-//                            emptyList(),
-//                            { throw IllegalArgumentException("Should not be called") })
-//
-//                    command.contains("AND") -> {
-//                        val (x, y) = command.split(" AND ")
-//                        val l = literals(x, y)
-//                        Wire(wire, instruction, null, wireNames(x, y)) {
-//                            val vals = l + it
-//                            vals[0] and vals[1]
-//                        }
-//                    }
-//
-//                    command.contains("OR") -> {
-//                        val (x, y) = command.split(" OR ")
-//                        val l = literals(x, y)
-//                        Wire(wire, instruction, null, wireNames(x, y)) {
-//                            val vals = l + it
-//                            vals[0] or vals[1]
-//                        }
-//                    }
-//
-//                    command.contains("LSHIFT") -> {
-//                        val (x, y) = command.split(" LSHIFT ")
-//                        Wire(wire, instruction, null, listOf(x), { it[0] shl y.toInt() })
-//                    }
-//
-//                    command.contains("RSHIFT") -> {
-//                        val (x, y) = command.split(" RSHIFT ")
-//                        Wire(wire, instruction, null, listOf(x), { it[0] shr y.toInt() })
-//                    }
-//
-//                    command.contains("NOT") -> {
-//                        val x = command.split("NOT ")[1]
-//                        Wire(wire, instruction, null, listOf(x), { it[0].inv() and 0xffff })
-//                    }
-//
-//                    else -> Wire(wire, instruction, null, listOf(command), { it[0] })
-//                }
-//            }.associateBy { it.name }
-
-//        val inactive = wires.values.filter { it.value == null }.toMutableSet()
 
     while (true) {
         val wire = wires.values.firstOrNull { w -> w.value == null && w.needs.all { wires[it]!!.value != null } }
